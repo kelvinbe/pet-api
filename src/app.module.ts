008ed00c9dae25956e824cc08a076525029db7a0
kelvin beno <kelvinbeno526@gmail.com>
2019-03-27 17:07:23 +0300

    Setup error handlers

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,21 @@ import {TypeOrmModule} from '@nestjs/typeorm'
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PetModule } from './pet/pet.module';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
+import { HttpErrorFilter } from './shared/http-error.filter';
+
 
 @Module({
   imports: [TypeOrmModule.forRoot(), PetModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+    provide: APP_FILTER,
+    useClass: HttpErrorFilter
+  },
+
+],
+   
 })
 export class AppModule {}
